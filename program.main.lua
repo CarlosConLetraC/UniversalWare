7f7ee10dd1f6cad60bcd708691ebf532d0658bd9
CarlosConLetraC <coderprestitan@gmail.com>
2026-08-22 20:26:07 -0600

    Ajustes

--- program.main.lua
+++ program.main.lua
@@ -1,26 +1,22 @@
 import("cjob")
 
 cjob.new(function()
-    for _ = 1, 5 do
-        io.write("pe")
+    for _ = 1, 5, 1 do
+        io.write("1\n")
+        io.write("3\n")
+        io.write("5\n")
+        io.write("7\n")
+        io.write("9\n")
     end
 end)
 
 cjob.new(function()
-    for _ = 1, 5 do
-        io.write("ne\n")
-    end
-end)
-
-cjob.new(function()
-    for _ = 1, 5 do
-        io.write("vag")
-    end
-end)
-
-cjob.new(function()
-    for _ = 1, 5 do
-        io.write("ina\n")
+    for _ = 1, 5, 1 do
+        io.write("2\n")
+        io.write("4\n")
+        io.write("6\n")
+        io.write("8\n")
+        io.write("10\n")
     end
 end)
 
